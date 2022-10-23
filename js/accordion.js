4822391a271986ef7d14b8de51e6fd3c514da707
andriitomilov <andriitomilov@ukr.net>
2022-10-23 17:54:14 +0300

    completed accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,5 +1,15 @@
-$(function () {
-  $("#accordion").accordion({
-    event: "mouseover",
-  });
+// $(function () {
+//   $("#accordion").accordion({
+//     event: "mouseover",
+//   });
+// });
+
+$(document).ready(function () {
+  $('.faq__subtitle').click(function (event) {
+    if ($('.faq__list').hasClass('accordion')) {
+      $(".faq__subtitle").not($(this)).removeClass('active');
+      $(".faq__text").not($(this).next()).slideUp(400);
+    }
+    $(this).toggleClass('active').next().slideToggle(400);
+  })
 });
